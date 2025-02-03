04afda89bf5ff5d82bc0d965e17c7a4c4006d03b
JLG-96 <jackleonardgill@gmail.com>
2025-02-03 19:56:29 +0000

    Updated README with details about team model, view for team information, and media configuration

--- cpd_site/cpd_site/settings.py
+++ cpd_site/cpd_site/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- cpd_site/team/admin.py
+++ cpd_site/team/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Team
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'manager', 'home_ground', 'founded_year', 'games_played', 'wins', 'draws', 'losses', 'goals_for', 'goals_against', 'points', 'logo')
+    search_fields = ['name', 'manager']
 
-admin.site.register(Team)
+
+admin.site.register(Team, TeamAdmin)
