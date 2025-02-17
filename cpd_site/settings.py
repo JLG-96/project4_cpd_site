082f56ba99ba1c235554125f82be8746e02dcbe7
JLG-96 <jackleonardgill@gmail.com>
2025-02-17 01:21:38 +0000

    DEBUG = False

--- cpd_site/settings.py
+++ cpd_site/settings.py
@@ -32,7 +32,7 @@ LOGOUT_REDIRECT_URL = '/'
 SECRET_KEY = config('SECRET_KEY', default='KnymOdiNz6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
